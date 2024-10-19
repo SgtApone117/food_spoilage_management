ce1025a6251881494d5f3e8cb82892eae4eda916
fruiteyloops <e_donley@u.pacific.edu>
2024-10-19 10:11:17 -0700

    setup test.py

--- .vscode/fast_api/main.py
+++ .vscode/fast_api/main.py
@@ -3,12 +3,21 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 from fastapi.openapi.docs import get_swagger_ui_html
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
 
 # Create FastAPI app instance
 app = FastAPI(debug=True, docs_url=None, redoc_url=None)
-
-# Mount the static files directory
 app.mount("/static", StaticFiles(directory="static", html=True))
+
+#Apparently fixes some CORS issue
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Custom Swagger documentation endpoint
 @app.get("/docs", include_in_schema=False)
@@ -20,8 +29,10 @@ async def custom_swagger_ui_html():
         swagger_js_url="static/swagger-ui-bundle.js",
         swagger_css_url="static/swagger-ui.css",
     )
+class StartModel(BaseModel):
+    s: str
 
-# Simple post request
-@app.post("/start")
-def start_app(s: str):
-    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"data": "test"})
+#request
+@app.post("/start", status_code=status.HTTP_200_OK)
+def start_app(start_data: StartModel):
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"data": start_data.s})
